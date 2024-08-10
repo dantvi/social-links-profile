ed8a74f0b8e34a5a5cbc381fac0fef4f2d7c13ee
dantvi <daniel.tving@gmail.com>
2024-08-10 18:25:23 +0200

    improved openLink function

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 // Opens the webpage in a new tab
-function gitHubLink() {
-    window.open('https://github.com/');
-}
-
-function frontendMentorLink() {
-    window.open('https://www.frontendmentor.io/');
-}
-
-function linkedInLink() {
-    window.open('https://www.linkedin.com/');
-}
-
-function twitterLink() {
-    window.open('https://x.com/');
-}
-
-function instagramLink() {
-    window.open('https://www.instagram.com/');
+function openLink(webpage) {
+    window.open(webpage);
 }
